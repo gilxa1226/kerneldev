86226af8eab98512c7ff1c371ec8a37e691967c4
Michael J. Haller <mike.haller@gmail.com>
2014-05-02 09:41:49 -0400

    Completed Tutorial section 2: Genesis

---
+++ src/main.c
@@ -0,0 +1,10 @@
+// main.c -- Defines the C-code kernel entry point, calls initialisation routines.
+// Made for JamesM's tutorials
+
+int main(struct multiboot *mboot_ptr)
+{
+	// All our initialization calls will go here.
+	
+
+	return 0xDEADBABA;
+}
