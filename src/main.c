9ef62ea07eb3550bc8853b006bf5a8bda3b0b2ef
Michael J. Haller <mike.haller@gmail.com>
2014-05-05 12:38:50 -0400

    Tutorial Section 3. The Screen - primary section completed.

--- src/main.c
+++ src/main.c
@@ -1,10 +1,18 @@
 // main.c -- Defines the C-code kernel entry point, calls initialisation routines.
 // Made for JamesM's tutorials
 
+#include "common.h"
+#include "monitor.h"
+
 int main(struct multiboot *mboot_ptr)
 {
 	// All our initialization calls will go here.
 	
+	// Initialize the screen (by clearning it)
+	monitor_clear();
+	// Write out a sample string.
+	monitor_write("Hello, world!");
+	
 
-	return 0xDEADBABA;
+	return 0;
 }
