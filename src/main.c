fa0c16bdeae09ddc24d423af7acd632956e57553
Michael J. Haller <mike.haller@gmail.com>
2014-05-08 14:59:06 -0400

    Completed Section 4. GDT and IDT Tutorial

--- src/main.c
+++ src/main.c
@@ -3,19 +3,20 @@
 
 #include "common.h"
 #include "monitor.h"
+#include "descriptor_tables.h"
 
 int main(struct multiboot *mboot_ptr)
 {
 	// All our initialization calls will go here.
+    init_descriptor_tables();
 	
 	// Initialize the screen (by clearning it)
 	monitor_clear();
 	// Write out a sample string.
-	monitor_write("Hello, world!");
-	monitor_write("\n");
-	monitor_write_hex(16);
-	monitor_write("\n");
-	monitor_write_dec(235);
+    monitor_write("Hello, world!\n");
+
+    asm volatile ("int $0x3");
+    asm volatile ("int $0x4");
 	
 
 	return 0;
