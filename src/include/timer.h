c020fbb6151ce8daabd4f9c527de7395b13f4b74
Michael J. Haller <mike.haller@gmail.com>
2014-05-08 16:17:59 -0400

    Section 5. IRQs and the PIT - Needs to be debugged, not fully working yet

---
+++ src/include/timer.h
@@ -0,0 +1,11 @@
+// timer.h -- Defines the interface for all PIT-related functions.
+// Written for JamesM's kernel development tutorials
+
+#ifndef __TIMER_H
+#define __TIMER_H
+
+#include "common.h"
+
+void init_timer(u32int frequency);
+
+#endif // __TIMER_H
--- src/main.c
+++ src/main.c
@@ -4,20 +4,20 @@
 #include "common.h"
 #include "monitor.h"
 #include "descriptor_tables.h"
+#include "timer.h"
 
 int main(struct multiboot *mboot_ptr)
 {
 	// All our initialization calls will go here.
     init_descriptor_tables();
+    init_timer(50);
 	
 	// Initialize the screen (by clearning it)
 	monitor_clear();
 	// Write out a sample string.
     monitor_write("Hello, world!\n");
 
-    asm volatile ("int $0x3");
-    asm volatile ("int $0x4");
-	
+    	
 
 	return 0;
 }
